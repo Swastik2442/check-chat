ca785ee4f6e807070253f7e0c8b247220d5b167f
Swastik Kulshreshtha <56737344+Swastik2442@users.noreply.github.com>
2025-08-19 22:57:24 +0530

    Style Changes

--- src/components/markdownRenderer.tsx
+++ src/components/markdownRenderer.tsx
@@ -43,33 +43,34 @@ const components: Components = {
   h4: (props) => <OverrideComponentClassName tag="h4" props={props} className="text-md font-bold" />,
   h5: (props) => <OverrideComponentClassName tag="h5" props={props} className="text-sm font-bold" />,
   h6: (props) => <OverrideComponentClassName tag="h6" props={props} className="text-xs font-bold" />,
+  ul: (props) => <OverrideComponentClassName tag="ul" props={props} className="ml-4 list-disc" />,
+  ol: (props) => <OverrideComponentClassName tag="ol" props={props} className="ml-4 list-decimal" />,
   code(props) {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const {children, className, node, ...rest} = props;
     const match = /language-(\w+)/.exec(className || '');
-    const codeText = String(children).replace(/\n$/, '');
+    if (!match) return (
+      <code {...rest} className={className}>
+        {children}
+      </code>
+    );
+
     return (
       <div className="border border-[rgb(40,44,52)] rounded-md my-[0.5em]">
         <div className="bg-[rgb(40,44,52)] px-[1em] pt-1 flex justify-between items-center">
           <span className="text-xs">{match && match[1]}</span>
-          {codeText && <CopyText text={codeText} className="text-sm hover:cursor-pointer hover:bg-[rgb(50,54,62)]" />}
+          <CopyText text={String(children)} className="text-sm hover:cursor-pointer hover:bg-[rgb(50,54,62)]" />
         </div>
-        {match ? (
-          // @ts-expect-error Expects child class when providing parent
-          <SyntaxHighlighter
-            {...rest}
-            PreTag="div"
-            customStyle={{ margin: "0" }}
-            language={match[1]}
-            style={oneDark}
-          >
-            {codeText}
-          </SyntaxHighlighter>
-        ) : (
-          <code {...rest} className={className}>
-            {children}
-          </code>
-        )}
+        {/* @ts-expect-error Expects child class when providing parent */}
+        <SyntaxHighlighter
+          {...rest}
+          PreTag="div"
+          customStyle={{ margin: "0" }}
+          language={match[1]}
+          style={oneDark}
+        >
+          {children}
+        </SyntaxHighlighter>
       </div>
     );
   }
